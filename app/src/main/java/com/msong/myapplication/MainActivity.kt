6b3b8fc92a71d59f40ff436a2ca6535e2c84dcc4
Ze Song <200395429@student.georgianc.on.ca>
2020-10-02 13:09:25 -0400

    final commit

--- app/src/main/java/com/msong/myapplication/MainActivity.kt
+++ app/src/main/java/com/msong/myapplication/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
     // vars for calculation
     var amount = 0.0
-    var tipPercent = 0.0
+    var tipPercent = 0.15
     var tip = 0.0
     var total = 0.0
 
@@ -21,17 +23,50 @@ class MainActivity : AppCompatActivity() {
         // When amount is changed
         editTextAmount.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(s: Editable?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                TODO("Not yet implemented")
+                amount = if (editTextAmount.text.isNotEmpty()) editTextAmount.text.toString().toDouble() else 0.0
+                calculate()
+
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                TODO("Not yet implemented")
+
             }
 
-        }
+        })
+
+        // when tip % is changed
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if (editTextAmount.text.isEmpty()) {
+                    return
+                }
+                textViewTipPercent.setText(p1.toString() + "%")
+                tipPercent = (seekBar / 100.00)
+                calculate()
+            }
+
+            override fun onStartTrackingTouch(p1: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p1: SeekBar?) {
+
+            }
+        })
+    }
+    fun calculate() {
+        // calc & display tip and total amounts
+        tip = amount * tipPercent
+        total = amount * tip
+
+        val currencyFormat = NumberFormat.getCurrencyInstance()
+
+        // display in the text views
+        textViewTipAmount.setText(currencyFormat.format(tip).toString())
+        textViewTotalAmount.setText(currencyFormat.format(total).toString())
     }
 }
